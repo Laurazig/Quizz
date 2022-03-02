79e21de42eabe0c2ff29fedef51a890de8838888
Laura <lhzig@protpnmail.com>
2022-03-02 16:38:21 +0100

    added notes

--- game.js
+++ game.js
@@ -1,3 +1,6 @@
+//created in stage 3
+//grab ID & classes from game.html -DOM querying
+//he talks about snippets here
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
 const progressText = document.getElementById('progressText');
@@ -5,14 +8,16 @@ const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById('progressBarFull');
 const loader = document.getElementById('loader');
 const game = document.getElementById('game');
+//************************* */
+//define variable
 let currentQuestion = {};
-let acceptingAnswers = false;
+let acceptingAnswers = false; //create a delay
 let score = 0;
 let questionCounter = 0;
-let availableQuesions = [];
+let availableQuesions = []; //copy of full Q set
+let questions = []; //s3 populated with 3 multiple Choice Qs
 
-let questions = [];
-
+//Stage 11 - import 10 geography questions
 fetch(
     'https://opentdb.com/api.php?amount=10&category=22&difficulty=hard&type=multiple'
 )
@@ -46,14 +51,14 @@ fetch(
         console.error(err);
     });
 
-//CONSTANTS
-const CORRECT_BONUS = 10;
+//CONSTANTS  S3
+const CORRECT_BONUS = 10; //correct answer
 const MAX_QUESTIONS = 3;
-
-startGame = () => {
-    questionCounter = 0;
+//S3 - 2 functions
+startGame = () => {  
+    questionCounter = 0; //reset
     score = 0;
-    availableQuesions = [...questions];
+    availableQuesions = [...questions]; //spread operator - full copy of Qs
     getNewQuestion();
     game.classList.remove('hidden');
     loader.classList.add('hidden');
@@ -70,12 +75,12 @@ getNewQuestion = () => {
     //Update the progress bar
     progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
-    const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+    const questionIndex = Math.floor(Math.random() * availableQuesions.length);  //based on length of array
     currentQuestion = availableQuesions[questionIndex];
     question.innerText = currentQuestion.question;
 
-    choices.forEach((choice) => {
-        const number = choice.dataset['number'];
+    choices.forEach((choice) => {     //iterate through choices
+        const number = choice.dataset['number'];  //data on HTML
         choice.innerText = currentQuestion['choice' + number];
     });
 
